34bcf6f59ad6f729a08f69c1c9ff50581f1d7f6f
Jiya Makhija <jmakhija@ucsd.edu>
2026-05-06 17:42:31 -0700

    fixed errors

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import os
-import numpy
 def main():
     print("Hello from dsc190-assignment-05!")
 
 x=5+6
 school='ucsd'
 def foo(a,b,c):
-    unused = 54
     return None
-if school == True:
+if school:
     print("bad")
 
 if __name__ == "__main__":
