7fcf50f23718985be20386d0b9f844ccffb84819
Jiya Makhija <jmakhija@ucsd.edu>
2026-05-06 17:37:52 -0700

    break things

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import os
+import numpy
 def main():
     print("Hello from dsc190-assignment-05!")
 
+x=5+6
+school='ucsd'
+def foo(a,b,c):
+    unused = 54
+    return None
+if school == True:
+    print("bad")
 
 if __name__ == "__main__":
     main()
